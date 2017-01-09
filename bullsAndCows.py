52065fbfbebf5cafa0457211a642a14ea04c1669
xklain <xklain@fi.muni.cz>
2017-01-09 23:08:13 +0100

    compare works correctly, digits is strings

--- bullsAndCows.py
+++ bullsAndCows.py
@@ -1,51 +1,49 @@
 from random import randint, choice
 
-"""
-returns a number to guess
-"""
+
 def get_number():
-    digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] #list of digits to choose from
-    a = randint(1, 9)
+    """ Returns a number to guess """
+    digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] #list of digits to choose from
+    a = str(randint(1, 9))
     digits.remove(a)
-    number = str(a)
+    number = a
 
     for i in range (0, 3):
         #chooses one of the numbers froom list nd removes it from the list
         a = (choice(digits))
-        number += str(a)
+        number += a
         digits.remove(a)
 
     return number
 
-"""
-players turn
-asks for a input from player and checks if it's valid
-calls compare_number()
-returns the result of compare_number()
-"""
+
 def player_turn(number):
+    """
+    players turn,
+    asks for an input from player and checks if it's valid,
+    returns the result of compare_number()
+    """
     guess = input("Your guess: ")
 
     if not guess.isdigit():     #checks if number
-        print("Enter a number dumbass")
+        print("That's not a number!")
     elif len(guess) != 4:       #checks the length
-        print("Number has less or more digits")
+        print("Number must have 4 digits, try again: ")
     else:
         return compare_number(number, guess)
 
-"""
-compares the number to be guessed with number from player
-counts cows and bulls and prints the result
-returns True if numbers are equal (if bulls == 4), False otherwise
-"""
+
 def compare_number(number, guess):
+    """
+    compares the number to be guessed with number from player
+    counts cows and bulls and prints the result
+    returns True if numbers are equal (if bulls == 4), False otherwise
+    """
     cows = 0
     bulls = 0
     #checks the number
     for i in range (4):
-        # if int(guess[i]) in number:
-        if guess[i] in number:
-            # if int(guess[i]) == number[i]:
+        if number[i] in guess:
             if guess[i] == number[i]:
                 bulls += 1
             else:
@@ -56,28 +54,24 @@ def compare_number(number, guess):
     return bulls == 4
 
 
-"""
-game function
-calls get_number()
-
-"""
 def game():
-    print("Hey fucker! Got a number for you! Guess what it is!")
+    """
+    game function
+    """
+    print("Hey! Got a number for you! Guess what it is!")
     number = get_number()
 
-    print (number) '''DEBUG'''
+    print (number)
 
     win = False
-    ofGuesses = 0
+    of_guesses = 0
     while not win:
         win = player_turn(number)
-        ofGuesses += 1
-    print('You done it you lucky basard! Guesses: %d' % ofGuesses)
+        of_guesses += 1
+    print('You done it you lucky bastard! Guesses: %d' % of_guesses)
 
 
-#
 game()
-
 
 # TODO: CLASSSES???
 # TODO: pydoc
